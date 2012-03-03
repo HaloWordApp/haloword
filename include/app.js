5168dbffc2e299433bee8ba91d6ba473e74057b1
Xhacker Liu <liu.dongyuan@gmail.com>
2012-03-03 22:43:53 +0800

    Bump version.

--- include/app.js
+++ include/app.js
@@ -38,12 +38,12 @@ $(document).ready(function() {
     });
 
     /* Update notification */
-    cur_version = 5;
+    cur_version = 6;
     if (!localStorage['prev_version']) {
         localStorage['prev_version'] = 0;
     }
     if (cur_version > localStorage['prev_version']) {
-        $("#bubble").html('<p>恭喜！您的 Halo Word 已更新至最新版本。由于 Google 字典不再提供中文释义，我们添加了来自<a target="blank" href="http://dict.cn"><strong>Dict.cn</strong></a>的中文释义。</p><p style="margin-top: 4px;">如果您喜欢这个应用，不妨考虑<a href="https://chrome.google.com/webstore/detail/bhkcehpnnlgncpnefpanachijmhikocj" target="_blank">打个五星</a>或者<a href="https://me.alipay.com/xhacker" target="_blank">捐赠</a>。</p><p class="align-right"><button  id="button-go-version">查看版本信息</button><button id="button-close-bubble">关闭</button></p>');
+        $("#bubble").html('<p>恭喜！您的 Halo Word 已更新至最新版本，支持在本地网页、Gmail 等所有页面进行取词。</p><p style="margin-top: 4px;">如果您喜欢这个应用，不妨考虑<a href="https://chrome.google.com/webstore/detail/bhkcehpnnlgncpnefpanachijmhikocj" target="_blank">打个五星</a>或者<a href="https://me.alipay.com/xhacker" target="_blank">捐赠</a>。</p><p class="align-right"><button  id="button-go-version">查看版本信息</button><button id="button-close-bubble">关闭</button></p>');
         $("#bubble").show();
     }
     $("#button-close-bubble").click(function() {
