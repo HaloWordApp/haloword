7fd879270631b5bd012106fe91c9b84c3199953f
chaofei <chaofeis@gmail.com>
2017-01-08 19:50:10 +0800

    Open url in same tab

--- include/context_menu.js
+++ include/context_menu.js
@@ -1,5 +1,14 @@
+var tabId = false;
+
 function contextMenuOnClick(info, t) {
-    chrome.tabs.create({url: "main.html#" + info.selectionText});
+    if (tabId === false) {
+      chrome.tabs.create({url: "main.html#" + info.selectionText}, function(tab) {
+        tabId = tab.id;
+      });
+    }
+    else {
+      chrome.tabs.update(tabId, {url: "main.html#" + info.selectionText, active: true});
+    }
 }
 
 var id = chrome.contextMenus.create({"title": "用 Halo Word 查询", "contexts":["selection"], "onclick": contextMenuOnClick});
